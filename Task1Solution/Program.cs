0dc5fbbdd5f259a810e17e54a9e81ad64a8c9da5
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-02 19:51:03 +0400

    Study and test

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -4,8 +4,26 @@
     {
         static void Main(string[] args)
         {
+            /*
             //1:
-            Console.ReadLine("Enter: ")
+            Console.WriteLine("Enter: ");
+            object obj = Console.ReadLine();
+            Console.WriteLine("He" + obj);
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old.");
+
+            for (int i=0; i<=5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            string[] fr = { "Apple", "Banana", "Cherry" };
+            foreach (string fruit in fr)
+            {
+                Console.WriteLine(fruit);
+            }
         }
     }
 }
