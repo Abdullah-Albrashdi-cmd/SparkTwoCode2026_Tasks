501653911a9d128653ca930fef25c2ec3721750b
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-05 14:25:37 +0400

    Test Task1

--- SP_1 Session 3 Practice Task/Program.cs
+++ SP_1 Session 3 Practice Task/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task1:  Absolute Difference
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double differ = Math.Abs(num1 - num2);
+            Console.WriteLine("The absolute difference between " +num1+" and "+num2+" is: "+differ);
+            Console.WriteLine();
+
+
+
         }
     }
 }
