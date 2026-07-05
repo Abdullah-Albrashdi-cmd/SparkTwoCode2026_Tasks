a088505f50814d3e36566b2f6cd902ed265c7247
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-05 14:33:10 +0400

    Test Task2

--- SP_1 Session 3 Practice Task/Program.cs
+++ SP_1 Session 3 Practice Task/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             //Task1:  Absolute Difference
             Console.WriteLine("Enter the first number:");
             double num1 = Convert.ToDouble(Console.ReadLine());
@@ -13,7 +14,14 @@
             double differ = Math.Abs(num1 - num2);
             Console.WriteLine("The absolute difference between " +num1+" and "+num2+" is: "+differ);
             Console.WriteLine();
-            
+            */
+
+            //Task2:   Power & Root Explorer
+            Console.WriteLine("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The powor of " + number + " is: " + Math.Pow(number,2));
+            Console.WriteLine("The square root of " + number + " is: " + Math.Sqrt(number));
+            Console.WriteLine();
 
 
         }
