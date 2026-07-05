0739e3ac1bd868204f07652a61e16157adead2fe
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-05 18:14:12 +0400

    Test Task4

--- SP_1 Session 3 Practice Task/Program.cs
+++ SP_1 Session 3 Practice Task/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace SP_1_Session_3_Practice_Task
+﻿using System.Data;
+
+namespace SP_1_Session_3_Practice_Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            /*
             //Task1:  Absolute Difference
             Console.WriteLine("Enter the first number:");
             double num1 = Convert.ToDouble(Console.ReadLine());
@@ -31,6 +33,19 @@
             Console.WriteLine("Your name in lowercase is: " + fullName.ToLower());
             Console.WriteLine("Your name has " + fullName.Length + " characters.");
             Console.WriteLine();
+            */
+
+            //Task4:   Subscription End Date
+            Console.WriteLine("Enter the number of days of a free trial: ");
+            int freeTrialDays = Convert.ToInt32(Console.ReadLine());
+
+            DateTime startData = DateTime.Today;
+
+            DateTime endDate = startData .AddDays(freeTrialDays);
+
+            Console.WriteLine("Trial end date: "+endDate.ToString("yyyy-MM-dd"));
+            Console.WriteLine();
+
 
         }
     }
