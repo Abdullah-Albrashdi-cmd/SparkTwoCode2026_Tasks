c658bfdede3d8c9a7aaeefe390d837d2a6db3952
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-05 14:19:48 +0400

    Create the solution file

---
+++ SP_1 Session 3 Practice Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SP_1_Session_3_Practice_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
