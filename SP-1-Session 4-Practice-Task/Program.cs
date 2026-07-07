b43efdf89ff7eee41691e9d0ad663d5b3c58ac21
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-07 14:19:23 +0400

    test task2

--- SP-1-Session 4-Practice-Task/Program.cs
+++ SP-1-Session 4-Practice-Task/Program.cs
@@ -2,18 +2,38 @@
 {
     internal class Program
     {
+        //Task1
         static void PrintWelcome(string name)
         {
             Console.WriteLine("Welcome " + name + " We're happy to have you here.");
+        } 
+
+        //Task2
+        static int SQUARE(int number)
+        {
+            return number * number;            
         }
 
-        
+
+
         static void Main(string[] args)
         {
+            //Task1
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
 
             PrintWelcome(userName);
+
+            //Task2
+            Console.WriteLine("Enter an interger number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int reselt = SQUARE(number);
+            Console.WriteLine("The Square of " + number + " is: " + reselt);
+            
+
+
+
         }
     }
 }
