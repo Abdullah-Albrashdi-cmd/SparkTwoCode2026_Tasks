6d885d277fd81151baaf5b208ffa8ac1c47dfe67
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-08 16:11:25 +0400

    Test task1

---
+++ SP-1-Session-5-Practice-Task-C#-Fundamentals-Part-5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SP_1_Session_5_Practice_Task_C__Fundamentals_Part_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1: Fixed Grades Array
+            int[] garde = new int[5];
+            for (int i = 0; i < garde.Length; i++) 
+            {
+                Console.WriteLine("Enter the grase: ");
+                garde[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int i in garde)
+            {
+                Console.WriteLine("The grades are: ");
+                Console.WriteLine(i);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
