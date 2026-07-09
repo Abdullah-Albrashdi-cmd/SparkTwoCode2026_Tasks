190d80e9b478c7e05528e2bbd37ef8f5e1853fe9
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-09 15:17:26 +0400

    Create the project file

---
+++ C# Fundamentals - Mini Compound Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Fundamentals___Mini_Compound_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
