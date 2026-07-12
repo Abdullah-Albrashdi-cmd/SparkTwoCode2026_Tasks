58c6194efc207123e146d941746821e8930e885f
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-12 23:41:54 +0400

    class BankAccount done

--- SP_2-OOP Part 1/Program.cs
+++ SP_2-OOP Part 1/Program.cs
@@ -2,9 +2,51 @@
 {
     internal class Program
     {
+        class BankAccount
+        {
+
+            public int AccountNumber { get; set; }
+            public string HolderName { get; set; }
+            public double Balance { get; set; }
+
+            public void Deposit(double amount)
+            {
+                Balance += amount;
+                SendEmail();
+            }
+
+            public void Withdraw(double amount)
+            {
+                if (Balance >= amount)
+                {
+                    Balance -= amount;
+                }
+                SendEmail();
+            }
+
+            public double CheckBalance()
+            {
+                PrintInformation();
+                return Balance;
+            }
+
+            private void PrintInformation()
+            {
+                Console.WriteLine("Holder Name: " + HolderName);
+                Console.WriteLine("Balance: " + Balance);
+            }
+
+            private void SendEmail()
+            {
+                Console.WriteLine("Email sent to " + HolderName + ".");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
+    
+    
+
