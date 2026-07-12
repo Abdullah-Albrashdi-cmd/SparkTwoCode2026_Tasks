441f65f966072debdf657f4d6f8e3a7a10a19add
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-12 23:31:07 +0400

    Create task file

---
+++ SP_2-OOP Part 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SP_2_OOP_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
