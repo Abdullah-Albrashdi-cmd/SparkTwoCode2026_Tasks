4a3c8b6eb8f0a512cd120d0e8ec5c82a7b460d9b
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-07-19 23:42:41 +0400

    add lists and the 6 rooms and the menu done

--- SP_2-OOP Part 2/Program.cs
+++ SP_2-OOP Part 2/Program.cs
@@ -63,7 +63,45 @@
     {
         static void Main(string[] args)
         {
+            List<Room> rooms = new List<Room>();
+            List<Guest> guests = new List<Guest>();
 
+            //load rooms
+            rooms.Add(new Room(101, "Single", 25.3, true));
+            rooms.Add(new Room(102, "Single", 26.0, true));
+            rooms.Add(new Room(201, "Double", 40.2, true));
+            rooms.Add(new Room(202, "Double", 45.0, true));
+            rooms.Add(new Room(301, "Suite", 91.0, true));
+            rooms.Add(new Room(302, "Suite", 111.1, true));
+
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("================================================");
+                Console.WriteLine("GRAND VISTA HOTEL — MANAGEMENT SYSTEM");
+                Console.WriteLine("================================================");
+                Console.WriteLine(" 1. Add New Room");
+                Console.WriteLine(" 2. Register New Guest");
+                Console.WriteLine(" 3. Book a Room for a Guest");
+                Console.WriteLine(" 4. View All Rooms");
+                Console.WriteLine(" 5. View All Guests");
+                Console.WriteLine(" 6. Search & Filter Rooms");
+                Console.WriteLine(" 7. Guest & Booking Statistics");
+                Console.WriteLine(" 8. Update Room Price");
+                Console.WriteLine(" 9. Guest Lookup by Name");
+                Console.WriteLine("10. Room Type Breakdown Report");
+                Console.WriteLine("11. Check Out a Guest");
+                Console.WriteLine("12. Remove Unavailable Rooms");
+                Console.WriteLine("13. Extend Guest Stay");
+                Console.WriteLine("14. Highest Revenue Booking");
+                Console.WriteLine("15. Guest Pagination Viewer");
+                Console.WriteLine(" 0. Exit");
+                Console.WriteLine("================================================");
+                Console.Write("Enter your choice: ");
+
+                
+            }
         }
     }
 }
